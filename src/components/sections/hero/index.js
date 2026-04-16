5445b73202d7865865a62795a1b4c4301cde549a
Eduardo Barbosa Silva <eduardobarbosasilva.oficial@outlook.com>
2026-04-16 15:25:24 -0300

    feat: seção hero concluida + ajustes de variáveis de maneira a promover uma melhor reutilização de código e reduzir boilerplate

---
+++ src/components/sections/hero/index.js
@@ -0,0 +1,5 @@
+import hero from './img/hero.png';
+
+const heroImage = document.querySelector('.js-hero-image');
+
+heroImage.src = hero;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import Style from './core/main.scss';
 import Header from './components/sections/header/index.js';
+import Hero from './components/sections/hero/index.js';
